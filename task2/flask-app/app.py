d28f6469ba03927d7b969118c712135439a1d435
Sujana <102030769+sujana95-26@users.noreply.github.com>
2023-09-29 16:20:34 +0100

    Update app.py

--- task2/flask-app/app.py
+++ task2/flask-app/app.py
@@ -1,20 +1,26 @@
-'''
-Basic static web app
-Use for exercises that don't need a database layer
-'''
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+# Replace [PASSWORD] with the root password for your mysql container
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{getenv("MYSQL_ROOT_PASSWORD")}@mysql:3306/flask-db'
+
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(30), nullable=False)
+	last_name = db.Column(db.String(30), nullable=False)
+	email = db.Column(db.String(150), nullable=False, unique=True)
+	def __repr__(self):
+		return ''.join(['User ID: ', str(self.id), '\r\n', 'Email: ', self.email, ' Name: ', self.first_name, ' ', self.last_name, '\n'])
+
 
 @app.route('/')
-def home():
-    hostname = getenv('HOSTNAME')
-    name = getenv('YOUR_NAME')
-    if name is None:
-      name = "friend"
-    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n"
+def hello():
+  data1 = Users.query.all()
+  return render_template('home.html', data1=data1)
 
 if __name__=='__main__':
-  app.run(host='0.0.0.0', port=5500, debug=True)
-
+  app.run(host='0.0.0.0', port=5000, debug=True)
